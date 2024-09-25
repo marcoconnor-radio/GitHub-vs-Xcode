1709c8faaad8cc0a3b44e19a450f306f4a5de8fa
Marc O'Connor <m.oconnor@radio.net>
2024-09-25 09:10:14 +0200

    Initial Commit

---
+++ Gitttttt/GittttttApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittttttApp.swift
+//  Gitttttt
+//
+//  Created by Marc O'Connor on 25.09.24.
+//
+
+import SwiftUI
+
+@main
+struct GittttttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GittttttTests/GittttttTests.swift
@@ -0,0 +1,17 @@
+//
+//  GittttttTests.swift
+//  GittttttTests
+//
+//  Created by Marc O'Connor on 25.09.24.
+//
+
+import Testing
+@testable import Gitttttt
+
+struct GittttttTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
